d632c59c65df782464be151fdf85beb370b8629c
Ramin Kaviani <raminkaviani@live.ca>
2024-06-26 12:21:13 -0400

    wip grav

--- src/main.rs
+++ src/main.rs
@@ -2,30 +2,45 @@ use bevy::prelude::*;
 use bevy_rapier2d::{
     control::KinematicCharacterController,
     dynamics::RigidBody,
-    geometry::Collider,
-    pipeline::QueryFilterFlags,
-    plugin::{NoUserData, RapierConfiguration, RapierPhysicsPlugin},
+    geometry::{Collider, Sensor},
+    pipeline::{CollisionEvent, QueryFilterFlags},
+    plugin::{NoUserData, RapierConfiguration, RapierContext, RapierPhysicsPlugin},
     render::RapierDebugRenderPlugin,
 };
 
 fn main() {
     let mut app = App::new();
     app.add_plugins(DefaultPlugins)
-        .insert_resource(RapierConfiguration {
-            gravity: Vec2::new(0., -300.),
-            ..Default::default()
-        })
+        // .insert_resource(RapierConfiguration {
+        //     gravity: Vec2::new(0., -300.),
+        //     ..Default::default()
+        // })
         .add_plugin(RapierPhysicsPlugin::<NoUserData>::pixels_per_meter(100.0))
         .add_plugin(RapierDebugRenderPlugin::default())
         .add_startup_system(setup)
-        .add_systems((handle_inputs, handle_velocity));
+        .add_systems((
+            handle_inputs,
+            handle_velocity,
+            handle_collisions
+                .before(handle_velocity)
+                .before(handle_inputs),
+        ));
 
     app.run();
 }
-const PLATFORM_SIZE: Vec2 = Vec2::new(2000.0, 50.0);
+const GRAVITY: f32 = -200.0;
+const PLATFORM_SIZE: Vec2 = Vec2::new(20000.0, 50.0);
 const SMALL_PLATFORM_SIZE: Vec2 = Vec2::new(300.0, 50.0);
+const MAX_PLAYER_VEL: f32 = 500.;
 #[derive(Component)]
 pub struct Player;
+
+#[derive(Component)]
+pub struct CollidedThisFrame(pub Timer);
+
+#[derive(Component)]
+
+pub struct Gravity(pub Vec2);
 
 #[derive(Component, Default)]
 pub struct Velocity(pub Vec2);
@@ -51,9 +66,10 @@ pub fn setup(mut commands: Commands) {
             filter_flags: QueryFilterFlags::EXCLUDE_SENSORS,
             ..default()
         })
-        .insert(RigidBody::Dynamic)
+        // .insert(RigidBody::Dynamic)
         .insert(Collider::cuboid(25.0, 50.0))
         .insert(Velocity::default())
+        .insert(Gravity(Vec2::new(0., GRAVITY)))
         .insert(Player);
 
     // Platform
@@ -67,7 +83,8 @@ pub fn setup(mut commands: Commands) {
             transform: Transform::from_translation(Vec3::new(0., -200., 0.)),
             ..default()
         })
-        .insert(Collider::cuboid(PLATFORM_SIZE.x / 2., PLATFORM_SIZE.y / 2.));
+        .insert(Collider::cuboid(PLATFORM_SIZE.x / 2., PLATFORM_SIZE.y / 2.))
+        .insert(Sensor);
     commands
         .spawn(SpriteBundle {
             sprite: Sprite {
@@ -81,7 +98,8 @@ pub fn setup(mut commands: Commands) {
         .insert(Collider::cuboid(
             SMALL_PLATFORM_SIZE.x / 2.,
             SMALL_PLATFORM_SIZE.y / 2.,
-        ));
+        ))
+        .insert(Sensor);
     commands
         .spawn(SpriteBundle {
             sprite: Sprite {
@@ -95,20 +113,20 @@ pub fn setup(mut commands: Commands) {
         .insert(Collider::cuboid(
             SMALL_PLATFORM_SIZE.x / 2.,
             SMALL_PLATFORM_SIZE.y / 2.,
-        ));
+        ))
+        .insert(Sensor);
 }
 
 pub fn handle_inputs(
-    mut player_query: Query<(Entity, &mut Velocity), (With<Player>,)>,
+    mut player_query: Query<(Entity, &mut Velocity, &mut Gravity), (With<Player>,)>,
     time: Res<Time>,
     key_input: ResMut<Input<KeyCode>>,
     mut commands: Commands,
 ) {
-    let (player_e, mut vel) = player_query.single_mut();
+    let (player_e, mut vel, mut grav) = player_query.single_mut();
     let mut d = Vec2::ZERO;
-    let speed = 500.;
+    let speed = 300.;
     let s = speed * time.delta_seconds();
-    let grav = Vec2::new(0., -200.);
 
     if key_input.pressed(KeyCode::A) {
         d.x -= 1.;
@@ -117,24 +135,35 @@ pub fn handle_inputs(
         d.x += 1.;
     }
     if key_input.pressed(KeyCode::W) {
-        d.y += 1.;
+        println!("W");
+        // d.y += 1.;
     }
     if key_input.pressed(KeyCode::S) {
         d.y -= 1.;
     }
     if key_input.just_pressed(KeyCode::Space) {
-        vel.0.y += 300.;
+        vel.0.y = 150.;
     }
 
     if d.x != 0. || d.y != 0. {
         d = d.normalize() * s;
     }
-    println!("{s:?} ||| {:?}, {grav:?}, {:?}", d, vel.0);
 
-    vel.0 += d + grav * time.delta_seconds();
-    if vel.0.y <= grav.y * 2. {
-        vel.0.y = grav.y * 2.;
+    if vel.0.y > 0. {
+        grav.0 = Vec2::new(0., GRAVITY);
     }
+    vel.0 += d;
+
+    if vel.0.x >= MAX_PLAYER_VEL {
+        vel.0.x = MAX_PLAYER_VEL;
+    }
+    if vel.0.x <= -MAX_PLAYER_VEL {
+        vel.0.x = -MAX_PLAYER_VEL;
+    }
+    if vel.0.y >= MAX_PLAYER_VEL {
+        vel.0.y = MAX_PLAYER_VEL;
+    }
+    // println!("{s:?} ||| {d:?}, {:?}", vel.0);
 
     // if d.x != 0. || d.y != 0. {
     //     player_kcc.translation = Some(vel.0);
@@ -142,10 +171,62 @@ pub fn handle_inputs(
 }
 
 pub fn handle_velocity(
-    mut player_query: Query<(&mut KinematicCharacterController, &Velocity), (With<Player>,)>,
+    mut player_query: Query<
+        (
+            Entity,
+            &mut KinematicCharacterController,
+            &mut Velocity,
+            &mut Gravity,
+            Option<&mut CollidedThisFrame>,
+        ),
+        (With<Player>,),
+    >,
     time: Res<Time>,
+    mut commands: Commands,
 ) {
-    for (mut transform, vel) in player_query.iter_mut() {
-        transform.translation = Some(vel.0 * time.delta_seconds());
+    for (e, mut transform, mut vel, mut grav, mut collided) in player_query.iter_mut() {
+        if let Some(mut collider) = collided {
+            if collider.0.percent() == 0. {
+                grav.0 = Vec2::ZERO;
+                collider.0.tick(time.delta());
+                // continue;
+            }
+            collider.0.tick(time.delta());
+            if collider.0.finished() {
+                grav.0 = Vec2::new(0., GRAVITY);
+
+                commands.entity(e).remove::<CollidedThisFrame>();
+            }
+        }
+        let mut new_vel = vel.0 + grav.0 * time.delta_seconds();
+        if new_vel.y <= grav.0.y * 2. {
+            new_vel.y = grav.0.y * 2.;
+        }
+        vel.0 = new_vel;
+        // println!("MOVING {:?} {:?}", new_vel, grav.0);
+        transform.translation = Some(new_vel * time.delta_seconds());
+    }
+}
+
+pub fn handle_collisions(
+    mut collisions: EventReader<CollisionEvent>,
+    mut player: Query<(Entity, &mut Velocity), (With<Player>, Without<CollidedThisFrame>)>,
+    colliders: Query<Entity, With<Collider>>,
+    mut commands: Commands,
+    context: ResMut<RapierContext>,
+) {
+    let Ok((player, mut vel)) = player.get_single_mut() else {
+        return;
+    };
+
+    let hits_this_frame = context
+        .intersection_pairs()
+        .filter(|c| (c.0 == player) || (c.1 == player));
+    for _hit in hits_this_frame {
+        // println!("{:?}", hit);
+        // vel.0 = Vec2::ZERO;
+        commands
+            .entity(player)
+            .insert(CollidedThisFrame(Timer::from_seconds(0.1, TimerMode::Once)));
     }
 }
